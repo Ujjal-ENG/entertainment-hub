2c85a985fb146d5ad2680bb87d4862f9779ddd78
user <ujjalroy7862@gmail.com>
2023-01-02 02:45:58 +0600

    previous most of the word are done, now work on SingleContent

---
+++ src/components/SingleContent/SingleContent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./SingleContent.css";
+
+const SingleContent = ({
+  id,
+  poster,
+  title,
+  date,
+  media_type,
+  vote_average,
+}) => {
+
+  return <div>
+    
+  </div>;
+};
+
+export default SingleContent;
--- src/pages/Trending.jsx
+++ src/pages/Trending.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+import SingleContent from "../components/SingleContent/SingleContent";
 
 const Trending = () => {
-  return (
-    <div>Trending</div>
-  )
-}
+  const [content, setContent] = useState([]);
+  const fetchTrending = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=849d1235b4f690bf48039d1e3bff9f5c`
+    );
+    setContent(data.results);
+  };
+  useEffect(() => {
+    fetchTrending();
+  }, []);
 
-export default Trending
+  return (
+    <div>
+      <span className="pageTitle">Trending</span>
+      <div className="trending">
+        {content &&
+          content.map((c) => {
+            return (
+              <SingleContent
+                key={c.id}
+                id={c.id}
+                poster={c.poster_path}
+                title={c.title || c.name}
+                date={c.release_date || c.first_air_date}
+                media_type={c.media_type}
+                vote_average={Math.round(c.vote_average)}
+              ></SingleContent>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default Trending;
