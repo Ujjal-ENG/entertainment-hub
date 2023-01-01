644bb8ea88d3d33aef7971a5702240b909b15eae
user <ujjalroy7862@gmail.com>
2023-01-02 01:52:10 +0600

    link the all pages using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Container } from "@mui/material";
 import "./App.css";
 import Header from "./components/Header/Header";
-import SimpleBottomNavigation from "./components/MainNav";
-import { Route, Routes } from "react-router-dom";
-import Trending from './pages/Trending'
-import Movies from "./pages/Movies";
-import Series from "./pages/Series";
-import Search from "./pages/Search";
 
+import SimpleBottomNavigation from "./components/MainNav";
+
+import { Route, Routes } from "react-router-dom";
+import Trending from "./pages/Trending.jsx";
+import Movies from "./pages/Movies.jsx";
+import Series from "./pages/Series.jsx";
+import Search from "./pages/Search.jsx";
 
 function App() {
   return (
@@ -16,10 +17,10 @@ function App() {
       <div className="App">
         <Container>
           <Routes>
-            <Route path="/" component={<Trending />} />
-            <Route path="/movies" component={<Movies />} />
-            <Route path="/series" component={<Series />} />
-            <Route path="/search" component={<Search />} />
+            <Route path="/" element={<Trending />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/series" element={<Series />} />
+            <Route path="/search" element={<Search />} />
           </Routes>
         </Container>
       </div>
--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
@@ -6,12 +6,23 @@ import MovieIcon from "@mui/icons-material/Movie";
 import LiveTvIcon from "@mui/icons-material/LiveTv";
 import SearchIcon from "@mui/icons-material/Search";
 import WhatshotIcon from "@mui/icons-material/Whatshot";
+import { useNavigate } from "react-router-dom";
+
 
 export default function SimpleBottomNavigation() {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
+
+  const history = useNavigate();
+
+  useEffect(() => {
+    if (value === 0) history("/");
+    else if (value === 1) history("/movies");
+    else if (value === 2) history("/series");
+    else history("/search");
+  }, [value, history]);
 
   return (
-    <Box sx={{ width: 500 }}>
+    <Box sx={{ width: 1,position: 'fixed', bottom: 0, left: 0, right: 0  }}>
       <BottomNavigation
         showLabels
         value={value}
@@ -26,7 +37,6 @@ export default function SimpleBottomNavigation() {
         <BottomNavigationAction label="Tv Show" icon={<LiveTvIcon />} />
 
         <BottomNavigationAction label="Search" icon={<SearchIcon />} />
-        
       </BottomNavigation>
     </Box>
   );
