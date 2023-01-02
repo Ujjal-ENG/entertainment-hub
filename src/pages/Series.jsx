8aecb8574c4f0c8483eb9551382a9c3acf0597bb
user <ujjalroy7862@gmail.com>
2023-01-02 21:36:34 +0600

    done the Series part completely

--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Genres from "../components/Genres";
+import CustomPagination from "../components/Pagination/CustomPagination";
+import SingleContent from "../components/SingleContent/SingleContent";
+import useGenre from "../hooks/useGenre";
 
 const Series = () => {
-  return (
-    <div>Series</div>
-  )
-}
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
 
-export default Series
+  const [numOfPage, setNumOfPage] = useState();
+
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [genres, setGenres] = useState([]);
+
+  const genreforURL = useGenre(selectedGenres);
+
+  const fetchMovies = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/discover/tv?api_key=849d1235b4f690bf48039d1e3bff9f5c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
+    );
+
+    setContent(data.results);
+    setNumOfPage(data.total_pages);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, [page, genreforURL]);
+
+  return (
+    <div>
+      <span className="pageTitle">Tv Series</span>
+      <Genres
+        type="tv"
+        selectedGenres={selectedGenres}
+        setSelectedGenres={setSelectedGenres}
+        genres={genres}
+        setGenres={setGenres}
+        setPage={setPage}
+      />
+      <div className="trending">
+        {content &&
+          content.map((c) => {
+            return (
+              <SingleContent
+                key={c.id}
+                id={c.id}
+                poster={c.poster_path}
+                title={c.title || c.name}
+                date={c.release_date || c.first_air_date}
+                media_type="tv"
+                vote_average={Math.round(c.vote_average)}
+              ></SingleContent>
+            );
+          })}
+      </div>
+      <CustomPagination setPage={setPage} numOfPages={numOfPage} />
+    </div>
+  );
+};
+
+export default Series;
